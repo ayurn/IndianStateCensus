3f901817a86364cb48b9ed828568c321c97b86d9
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-21 23:32:02 +0530

    Added : test case 1.2 returns a custom exception

--- src/test/java/StateCensusAnalyserTest.java
+++ src/test/java/StateCensusAnalyserTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 public class StateCensusAnalyserTest {
 
     public final String CSV_FILE_PATH = "C:\\Users\\AYUR\\IdeaProjects\\IndianStateCensus\\src\\main\\resources\\StateCensusData.csv";
+    public final String WRONG_CSV_FILE_PATH = "C:\\Users\\AYUR\\IdeaProjects\\IndianStateCensus\\src\\main\\resources\\StateCensusD.csv";
 
     private static StateCensusAnalyser censusAnalyser;
 
@@ -16,6 +17,17 @@ public class StateCensusAnalyserTest {
             Assertions.assertEquals(29, numberOfStates);
         } catch (Exception exception) {
             exception.getMessage();
+        }
+    }
+
+    // TC-1.2
+    @Test
+    public void givenWrongStateCsvFile_CheckPresentOrNot_ShouldReturnException() {
+        try {
+            StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+            censusAnalyser.loadCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException exception) {
+            Assertions.assertEquals("file is not found", exception.getMessage());
         }
     }
 }
