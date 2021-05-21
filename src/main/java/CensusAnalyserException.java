3e5efa87436662ca3462225b1213420e14c97792
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-21 18:34:37 +0530

    Initial commit

---
+++ src/main/java/CensusAnalyserException.java
@@ -0,0 +1,14 @@
+public class CensusAnalyserException extends Exception {
+
+    private final exceptionType type;
+
+    public enum exceptionType {
+        FILE_NOT_FOUND, WRONG_FILE
+    };
+
+    CensusAnalyserException(exceptionType type, String message) {
+        super(message);
+        this.type = type;
+    }
+
+}
